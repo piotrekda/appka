a1ddfd3a239d4de7b3be93d70d7a5ca149ae91e2
piotrekda <35435242+piotrekda@users.noreply.github.com>
2018-01-26 18:32:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/piotr/myapplication/tran.kt
@@ -0,0 +1,12 @@
+package com.example.piotr.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class tran : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tran)
+    }
+}
